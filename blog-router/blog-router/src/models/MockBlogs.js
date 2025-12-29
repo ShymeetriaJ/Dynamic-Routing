07f53e297240da505ce6b9ce92fa911999a3265d
ShymeetriaJ <sjeducates2@gmail.com>
2025-12-29 10:33:02 -0500

    added mock blog posts to MockBlogs.js.

--- blog-router/blog-router/src/models/MockBlogs.js
+++ blog-router/blog-router/src/models/MockBlogs.js
@@ -1,5 +1,24 @@
-function mockBlogs() {
-    
-}
+const posts = [
+  {
+    id: 1,
+    slug: "tech-aspirations",
+    title: "Tech Aspirations",
+    content: "All I knew was that technology was cool and that it could change my life. I remember when my family got our first computer and it was thought of like a tool that could help me do my homework."
+  },
+  {
+    id: 2,
+    slug: "the-dark-side-of-technology",
+    title: "The Dark Side of Technology",
+    content: "For my generation, the internet came without rules or guidelines that could navigate one in a positive direction. They told our parents, this pc can help them do their projects and homework but unbeknownst to our parents, there was whole other world that would be a little more interesting than a digital encyclopedia."
+  },
+  {
+    id: 3,
+    slug: "then-and-now-in-the-net",
+    title: "Then and Now: In the Net",
+    content: "So when I really think about the world wide web or the internet, it makes me feel a little uneasy. It can be as harmless as a five year old watching Gracie's Corner but it could also send out a message that could cause a ripple effect of mass destruction in a matter of seconds."
+  }
+];
+
+export default posts;
 
 
